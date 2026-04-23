c677dab430cd2df010ebd05932d25a2089a110b7
griffithscal6-prog <griffithscal6@gmail.com>
2026-04-23 10:31:37 -0600

    moved several files to main

---
+++ other/hello-world.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+x = 131
+print(f"This is CSE, {x}")
+list = [2, 4, 6]
+print(list)
+age = int(input("What is your age?"))
+print(f"You are {age} years old.")
+for number in range(0, 10):
+    print(number)
